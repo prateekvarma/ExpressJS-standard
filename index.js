41d0166c3b062af91d43c2aeb2dabb94d1faaf9d
Prateek Varma <prtk.varma@gmail.com>
2021-02-25 09:50:20 -0600

    reading data from the json file and display on frontend

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => 
-    res.send(`a get response with / route at port ${PORT}`)
+    res.json(data)
 );
 
 app.post('/newItem', (req, res) => 
