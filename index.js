901b464caf33318464e366c7c3ede33e7800c6ea
Prateek Varma <prtk.varma@gmail.com>
2021-02-25 10:04:28 -0600

    serve static content on expressjs

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import data from './data/data.json';
 const app = express();
 const PORT = 3000;
 
+// this looks for a filder named public, and will serve any static file in public folder on the root url. Eg: localhost:3000/image.jpg
+app.use(express.static('public'));
+
+// this will serve static content on the images folder, with the url with images appended to it. Eg: localhost:3000/images/image.jog
+app.use('/images', express.static('images'));
+
 app.get('/', (req, res) => 
     res.json(data)
 );
