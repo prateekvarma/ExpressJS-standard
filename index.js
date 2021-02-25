2ac3fb268d5cfddeb5a6441f487368334d7a2d8d
Prateek Varma <prtk.varma@gmail.com>
2021-02-25 09:45:07 -0600

    basic API endpoints written, and added a JSON file for future test

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 import express from 'express';
+import data from './data/data.json';
 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => 
-    console.log(`Your server is running on port ${PORT}`)
+app.get('/', (req, res) => 
+    res.send(`a get response with / route at port ${PORT}`)
 );
+
+app.post('/newItem', (req, res) => 
+    res.send(`a post response with /newItem route at port ${PORT}`)
+);
+
+app.put('/item', (req, res) => 
+    res.send(`a put response with /item route at port ${PORT}`)
+);
+
+app.delete('/item', (req, res) => 
+    res.send(`a delete response with /item route at port ${PORT}`)
+);
+
+app.listen(PORT, () => {
+    console.log(`Your server is running on port ${PORT}`);
+    console.log(data);
+});
